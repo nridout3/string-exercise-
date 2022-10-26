56cbd252db8e77cc4798aa2b47626c2d4c21022a
niyaridout55@gmail.com <Niya Ridout>
2022-10-26 15:12:37 -0400

    create parseAndDisplay function

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,17 @@
-console.log('the script tag works');
+function parseAndDisplayName(name){
+    let space = name.indexOf(' ');
+    let firstName = name.substring(0, space);
+    let lastName = name.substring(space + 1);
+    console.log(firstName, lastName, name);
+}
+parseAndDisplayName('Bruce Wayne');
+
+//console.log('the script tag works');
+//let fullName = "Niya Ridout";
+//console.log(`the variable fullName is: ${fullName}`);
+//let space = fullName.indexOf(' ');
+//console.log(space);  //4
+//let firstName = fullName.substring(0, space);
+//console.log(firstName);
+//let lastName = fullName.substring(space + 1);
+//console.log(lastName);
